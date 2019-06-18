aa3f56087d0bfdd2d7511fb2b07622b567944e52
isso0424 <isso04240129@gmail.com>
2019-06-18 00:02:57 +0000

    adapter

--- registration/users/adapter,py.py
+++ registration/users/adapter,py.py
@@ -5,4 +5,4 @@ class MyAccountAdapter(DefaultAccountAdapter):
     def __init__(self):
         super().__init__(self, request=None)
     def get_login_redirect_url(self, request):
-        return 'http://127.0.0.1:8000/'
+        return 'https://isso4129.pythonanywhere.com/registration/home'
